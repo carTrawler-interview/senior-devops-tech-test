d82b16f6a48c2a5ea3a64f0cc53324f14f73c1ca
Cloud User <ec2-user@ip-192-168-83-157.eu-west-1.compute.internal>
2025-04-24 18:26:27 +0000

    testing

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.get('/', (req, res) => {
-res.send('Hello from the Node.js app!!! Completed');
+res.send('Hello from the Node.js app!!');
 });
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);

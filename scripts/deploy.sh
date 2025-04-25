62a7e37cf19dc59066d1b707d0a0a6666efe1644
nikilrao18 <116964505+nikilrao18@users.noreply.github.com>
2025-04-25 02:14:42 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Create if deleted
 echo "Installing dependencies for deployment"
+helm uninstall nodejs-app -n car-trawler-helm || true
 kubectl apply -f k8s/service.yaml || true
 kubectl apply -f k8s/ingress.yaml || true

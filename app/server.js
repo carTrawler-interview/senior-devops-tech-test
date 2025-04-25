a50d19e70df241d7b534bd088140939dbbfb8de7
nikilrao18 <116964505+nikilrao18@users.noreply.github.com>
2025-04-25 01:49:19 +0100

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
-app.get('/', (req, res) => {
+app.get('/helm', (req, res) => {
 res.send('Hello from the Node.js app!! You have successfully executed HELM pipeline');
 });
 app.listen(PORT, () => {
